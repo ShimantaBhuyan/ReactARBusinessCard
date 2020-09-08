b62b8d016858b71f742c595abd5c37f3b58b2da5
ShimantaBhuyan <bhuyanshimanta@gmail.com>
2020-09-08 22:35:27 +0530

    detection mode mono

--- loadingWithThree.js
+++ loadingWithThree.js
@@ -153,9 +153,9 @@ const initializeAR = () => {
 
     // create atToolkitContext
     var arToolkitContext = new THREEx.ArToolkitContext({
-        //detectionMode: 'mono',
-        detectionMode: 'color_and_matrix',
-        matrixCodeType: '3x3',
+        detectionMode: 'mono',
+        //detectionMode: 'color_and_matrix',
+        //matrixCodeType: '3x3',
         canvasWidth: 480,
         canvasHeight: 640,
     }, {
