56f3dd051ea81d7918c12d3c59fb5eb3b756bdf3
ShimantaBhuyan <bhuyanshimanta@gmail.com>
2020-09-13 13:16:34 +0530

    Commented erroneous code

--- ARCard.js
+++ ARCard.js
@@ -40,81 +40,80 @@ const registerClick = () => {
 }
 
 const initializeScene = (props) => {    
-    checkMedia()
+    //checkMedia()
 
-    AFRAME.registerComponent('markerhandler', {
-        init: function () {
-          this.el.sceneEl.addEventListener('markerFound', () => {
-            // redirect to custom URL
-            //window.location = 'https://github.com/AR-js-org/AR.js';
-            console.log("IMAGE TRACKED!")
-          })
-        }
-    })
+    // AFRAME.registerComponent('markerhandler', {
+    //     init: function () {
+    //       this.el.sceneEl.addEventListener('markerFound', () => {
+    //         // redirect to custom URL
+    //         //window.location = 'https://github.com/AR-js-org/AR.js';
+    //         console.log("IMAGE TRACKED!")
+    //       })
+    //     }
+    // })
 
-    var sceneEl = document.querySelector("a-scene")
-    sceneEl.flushToDOM(true)
+    //var sceneEl = document.querySelector("a-scene")
 
     // set scene attributes
-    AFRAME.registerComponent('arcardscene', {
-        init: function () {   
-            registerClick()
-            // get scene element
-            var sceneEl = this.el; 
-            // set header text attributes
-            sceneEl.querySelector('#headerText').setAttribute('rotation', {x: -90, y: -90, z: 90});
-            sceneEl.querySelector('#headerText').setAttribute('position', {x: -40, y: 0, z: -620});
-            sceneEl.querySelector('#headerText').setAttribute('text-geometry', {value: "AR Business Card", size: 15});
-            // set name text attributes
-            sceneEl.querySelector('#nameText').setAttribute('rotation', {x: -90, y: -90, z: 90});
-            sceneEl.querySelector('#nameText').setAttribute('position', {x: -100, y: 0, z: -450});
-            sceneEl.querySelector('#nameText').setAttribute('text-geometry', {value: props.user.name, font: `#optimerBoldFont`, size: 20});
-            // set logo plane attributes
-            sceneEl.querySelector('#avevaLogoPlane').setAttribute('rotation', {x: -90, y: -90, z: 90});
-            sceneEl.querySelector('#avevaLogoPlane').setAttribute('position', {x: 50, y: 0, z: -570});
-            sceneEl.querySelector('#avevaLogoPlane').setAttribute('height', 200);
-            sceneEl.querySelector('#avevaLogoPlane').setAttribute('width', 476);
-            // set portfolio block attributes
-            sceneEl.querySelector('#portfolioBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
-            sceneEl.querySelector('#portfolioBlock').setAttribute('position', {x: 180, y: 0, z: -400});
-            sceneEl.querySelector('#portfolioBlock').setAttribute('material', {src: `#headshotTexture`});
-            // set linkedin block attributes
-            sceneEl.querySelector('#linkedinBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
-            sceneEl.querySelector('#linkedinBlock').setAttribute('position', {x: -80, y: 0, z: -400});
-            sceneEl.querySelector('#linkedinBlock').setAttribute('material', {src: `#linkedinTexture`});
-            // set github block attributes
-            sceneEl.querySelector('#githubBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
-            sceneEl.querySelector('#githubBlock').setAttribute('position', {x: -80, y: 0, z: -145});
-            sceneEl.querySelector('#githubBlock').setAttribute('material', {src: `#githubTexture`});
-            // set email block attributes
-            sceneEl.querySelector('#emailBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
-            sceneEl.querySelector('#emailBlock').setAttribute('position', {x: 180, y: 0, z: -145});
-            sceneEl.querySelector('#emailBlock').setAttribute('material', {src: `#emailTexture`});
-            // set camera entity attributes   
-            sceneEl.querySelector('#cameraEntity').setAttribute('cursor', {rayOrigin: `mouse`});
-            // set cursor entity attributes           
-            sceneEl.querySelector('#cursorEntity').setAttribute('raycaster', {objects: `.clickable`});      
-            sceneEl.querySelector('#cursorEntity').setAttribute('cursor', {fuse: true, fuseTimeout: 2000});      
-            sceneEl.querySelector('#cursorEntity').setAttribute('position', {x: 0, y: 0, z: -2});
-            sceneEl.querySelector('#cursorEntity').setAttribute('scale', {x: 0.1, y: 0.1, z: 0.1});
-            sceneEl.querySelector('#cursorEntity').setAttribute('geometry', {primitive: `ring`});      
-            sceneEl.querySelector('#cursorEntity').setAttribute('material', {color: `white`, shader: `flat`}); 
+    // AFRAME.registerComponent('arcardscene', {
+    //     init: function () {   
+    //         registerClick()
+    //         // get scene element
+    //         var sceneEl = this.el; 
+    //         // set header text attributes
+    //         sceneEl.querySelector('#headerText').setAttribute('rotation', {x: -90, y: -90, z: 90});
+    //         sceneEl.querySelector('#headerText').setAttribute('position', {x: -40, y: 0, z: -620});
+    //         sceneEl.querySelector('#headerText').setAttribute('text-geometry', {value: "AR Business Card", size: 15});
+    //         // set name text attributes
+    //         sceneEl.querySelector('#nameText').setAttribute('rotation', {x: -90, y: -90, z: 90});
+    //         sceneEl.querySelector('#nameText').setAttribute('position', {x: -100, y: 0, z: -450});
+    //         sceneEl.querySelector('#nameText').setAttribute('text-geometry', {value: props.user.name, font: `#optimerBoldFont`, size: 20});
+    //         // set logo plane attributes
+    //         sceneEl.querySelector('#avevaLogoPlane').setAttribute('rotation', {x: -90, y: -90, z: 90});
+    //         sceneEl.querySelector('#avevaLogoPlane').setAttribute('position', {x: 50, y: 0, z: -570});
+    //         sceneEl.querySelector('#avevaLogoPlane').setAttribute('height', 200);
+    //         sceneEl.querySelector('#avevaLogoPlane').setAttribute('width', 476);
+    //         // set portfolio block attributes
+    //         sceneEl.querySelector('#portfolioBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
+    //         sceneEl.querySelector('#portfolioBlock').setAttribute('position', {x: 180, y: 0, z: -400});
+    //         sceneEl.querySelector('#portfolioBlock').setAttribute('material', {src: `#headshotTexture`});
+    //         // set linkedin block attributes
+    //         sceneEl.querySelector('#linkedinBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
+    //         sceneEl.querySelector('#linkedinBlock').setAttribute('position', {x: -80, y: 0, z: -400});
+    //         sceneEl.querySelector('#linkedinBlock').setAttribute('material', {src: `#linkedinTexture`});
+    //         // set github block attributes
+    //         sceneEl.querySelector('#githubBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
+    //         sceneEl.querySelector('#githubBlock').setAttribute('position', {x: -80, y: 0, z: -145});
+    //         sceneEl.querySelector('#githubBlock').setAttribute('material', {src: `#githubTexture`});
+    //         // set email block attributes
+    //         sceneEl.querySelector('#emailBlock').setAttribute('rotation', {x: -90, y: -90, z: 90});
+    //         sceneEl.querySelector('#emailBlock').setAttribute('position', {x: 180, y: 0, z: -145});
+    //         sceneEl.querySelector('#emailBlock').setAttribute('material', {src: `#emailTexture`});
+    //         // set camera entity attributes   
+    //         sceneEl.querySelector('#cameraEntity').setAttribute('cursor', {rayOrigin: `mouse`});
+    //         // set cursor entity attributes           
+    //         sceneEl.querySelector('#cursorEntity').setAttribute('raycaster', {objects: `.clickable`});      
+    //         sceneEl.querySelector('#cursorEntity').setAttribute('cursor', {fuse: true, fuseTimeout: 2000});      
+    //         sceneEl.querySelector('#cursorEntity').setAttribute('position', {x: 0, y: 0, z: -2});
+    //         sceneEl.querySelector('#cursorEntity').setAttribute('scale', {x: 0.1, y: 0.1, z: 0.1});
+    //         sceneEl.querySelector('#cursorEntity').setAttribute('geometry', {primitive: `ring`});      
+    //         sceneEl.querySelector('#cursorEntity').setAttribute('material', {color: `white`, shader: `flat`}); 
 
-            // need to set manually since attributes were not updating in DOM
-            sceneEl.flushToDOM(true);  // Flush every entity.
-        }
-    })
+    //         // need to set manually since attributes were not updating in DOM
+    //         sceneEl.flushToDOM(true);  // Flush every entity.
+    //     }
+    // })
 }
 
 const ARCard = (props) => {
     console.log(props)
 
-    useEffect(() => {
-        initializeScene(props)
-        return(() => {
-            initializeScene(props)
-        })
-    }) 
+    // useEffect(() => {
+    //     initializeScene(props)
+    //     return(() => {
+    //         initializeScene(props)
+    //     })
+    // }) 
 
     return(
         // <>
